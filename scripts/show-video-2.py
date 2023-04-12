fbf7fa88140ee8b76303cdca5253477794c665ec
official.tausif <tausif@cyberboxer>
2023-04-12 14:41:28 +0530

    capture video and store it into the file format

--- scripts/show-video-2.py
+++ scripts/show-video-2.py
@@ -1,32 +1,33 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
-fourcc = cv2.VideoWriter_fourcc(*'h264')
-out  = cv2.VideoWriter('images/video_output.mp4',fourcc,20,(1280,720))
 
-while (cap.isOpened()):
-    ret,frame  = cap.read()
+def show_video(cap,output):
 
-    # ret boolean value
-    
-    if ret:
+    while (cap.isOpened()):
+        ret,frame  = cap.read()
+        # ret boolean value
+        if ret:
 
-        print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-        out.write(frame)
-        # color live video
-        # cv2.imshow('frame',frame)
+            output.write(frame)
+            # color live video
+            # cv2.imshow('frame',frame)
+            # gray video
+            gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+            cv2.imshow('frame',gray)
 
-        # gray video
-        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        cv2.imshow('frame',gray)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
             break
-    else:
-        break
 
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(0)
+    fourcc = cv2.VideoWriter_fourcc(*'h264')
+    output  = cv2.VideoWriter('../images/video_output.mp4',fourcc,20,(1280,720))
+    show_video(cap,output)
+    cap.release()
+    output.release()
+    cv2.destroyAllWindows()
