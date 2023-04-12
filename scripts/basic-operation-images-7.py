a4c90e18c233f253bf7146616862d85c2c6f546b
official.tausif <tausif@cyberboxer>
2023-04-12 14:22:33 +0530

    script for basics operations like adding moving etc

--- scripts/basic-operation-images-7.py
+++ scripts/basic-operation-images-7.py
@@ -1,26 +1,34 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('../images/pic1.jpeg',1)
-img2  = cv2.imread('../images/pic2.jpeg',1)
 
-print(img.shape)
-print(img.size)
-print(img.dtype)
+def basic_operation():
 
-b,g,r = cv2.split(img)
-img = cv2.merge((b,g,r))
-pillow = img[584:1355,707:1537]
-pillow = cv2.resize(img[584:1355,707:1537], (242, 136))
-h, w = 136, 183
-pillow_resized = cv2.resize(pillow, (w, h))
-img[924:924+h, 1017:1017+w] = pillow_resized
+    img = cv2.imread('../images/pic1.jpeg',1)
+    img2  = cv2.imread('../images/pic2.jpeg',1)
 
-img=  cv2.resize(img, (512,512))
-img2 = cv2.resize(img2,(512,512))
-merge = cv2.addWeighted(img,0.35,img2,0.65,0)
-cv2.imshow('image',merge)
+    print(img.shape)
+    print(img.size)
+    print(img.dtype)
 
+    b,g,r = cv2.split(img)
+    img = cv2.merge((b,g,r))
 
-if cv2.waitKey(0) & 0xFF == ord('q'):
-    cv2.destroyAllWindows()
+    # move images or ROI from one place to another
+    pillow = img[584:1355,707:1537]
+    pillow = cv2.resize(img[584:1355,707:1537], (242, 136))
+    h, w = 136, 183
+    pillow_resized = cv2.resize(pillow, (w, h))
+    img[924:924+h, 1017:1017+w] = pillow_resized
+
+    #  add two image ac to the strength
+    img=  cv2.resize(img, (512,512))
+    img2 = cv2.resize(img2,(512,512))
+    merge = cv2.addWeighted(img,0.35,img2,0.65,0)
+    cv2.imshow('image',merge)
+
+    if cv2.waitKey(0) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    basic_operation()
