bd17ce18d8f0ebe61d0e8729179f8299be1f3ec3
official.tausif <tausif@cyberboxer>
2023-04-12 14:44:04 +0530

    load images uisng opencv

--- scripts/load-image-1.py
+++ scripts/load-image-1.py
@@ -1,11 +1,15 @@
 import cv2
 
-img = cv2.imread('images/pic2.jpeg',0)
-cv2.imshow('image',img)
+def load_images():
+    img = cv2.imread('images/pic2.jpeg',0)
+    cv2.imshow('image',img)
 
-key  = cv2.waitKey(5000)
-if key == 27:
-    cv2.destroyAllWindows()
-elif key == ord('s'):
-    cv2.imwrite('images/pic2_copy.png',img)
-    cv2.destroyAllWindows()
+    key  = cv2.waitKey(5000)
+    if key == 27:
+        cv2.destroyAllWindows()
+    elif key == ord('s'):
+        cv2.imwrite('images/pic2_copy.png',img)
+        cv2.destroyAllWindows()
+        
+if __name__ == "__main__":
+    load_images()
